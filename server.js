bc9d6c405d3dbdb204d6604e336e1867f8706d53
Gardenia04 <grdnia040617@gmail.com>
2024-12-22 04:52:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+// 引入必要的模組
 const http = require('http');
 const WebSocket = require('ws');
 const readline = require('readline');
+const path = require('path');
+const fs = require('fs');
 
 // 獲取 Fly.io 提供的動態 PORT
 const port = process.env.PORT || 8080;
 
 // 創建 HTTP 伺服器
 const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('WebSocket server is running');
+    const filePath = path.join(__dirname, 'public', 'index.html');
+
+    // 返回靜態文件（HTML）
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error loading index.html');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
 });
 
 // 啟動 HTTP 伺服器
@@ -19,11 +32,14 @@ server.listen(port, '0.0.0.0', () => {
 // 建立 WebSocket 伺服器
 const wss = new WebSocket.Server({ server });
 
+// 監聽 WebSocket 連線
 wss.on('connection', (ws) => {
     console.log('Client connected');
 
     ws.on('message', (message) => {
         console.log(`Received: ${message}`);
+
+        // 如果接收到 START 指令，進行廣播
         if (message === 'START') {
             broadcastCountdown();
         }
@@ -33,6 +49,7 @@ wss.on('connection', (ws) => {
         console.log('Client disconnected');
     });
 
+    // 初始消息發送給連接的客戶端
     ws.send('Welcome to WebSocket server!');
 });
 
@@ -52,6 +69,7 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+// 終端指令處理
 rl.on('line', (input) => {
     if (input === 'broadcastCountdown') {
         broadcastCountdown();
