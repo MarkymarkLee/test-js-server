ca96e5aaea98103984c6e94cb1ba263150c8df8e
Gardenia04 <grdnia040617@gmail.com>
2024-12-22 15:24:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,12 +24,7 @@ wss.on('connection', (ws) => {
 
     ws.on('message', (message) => {
         console.log(`Received: ${message}`);
-        // 如果接收到 START 指令，執行廣播
-        if (message === 'START') {
-            broadcastCountdown();
-        }
-        // 如果接收到 broadcastCountdown 指令，執行廣播
-        if (message === 'broadcastCountdown') {
+        if (message === 'START' || message === 'broadcastCountdown') {
             broadcastCountdown();
         }
     });
@@ -38,19 +33,21 @@ wss.on('connection', (ws) => {
         console.log('Client disconnected');
     });
 
-    // 向新連接的客戶端發送歡迎消息
     ws.send('Welcome to WebSocket server!');
 });
 
 // 廣播倒計時功能
 function broadcastCountdown() {
     console.log('Broadcasting START_COUNTDOWN to all clients...');
+    let sentCount = 0; // 用來記錄成功發送的客戶端數量
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
             client.send('START_COUNTDOWN');
             console.log('START_COUNTDOWN sent to a client');
+            sentCount++;
         }
     });
+    console.log(`Broadcasted to ${sentCount} clients.`);
 }
 
 // 使用 readline 接收終端輸入
@@ -60,7 +57,7 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', (input) => {
-    if (input === 'broadcastCountdown') {
+    if (input.trim() === 'broadcastCountdown') {
         broadcastCountdown();
     } else {
         console.log(`Unknown command: ${input}`);
